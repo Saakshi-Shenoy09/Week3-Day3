cb1eb0fc569eaf2e1c9a97caebe4a489334356c3
Saakshi-Shenoy09 <saakshi.shenoy@saks.com>
2025-05-06 14:49:52 +0530

    Update GlobalExceptionHandler.cs

--- Middleware/GlobalExceptionHandler.cs
+++ Middleware/GlobalExceptionHandler.cs
@@ -21,8 +21,7 @@ namespace Week3Day3.Middleware
                 await _next(context); 
             }
             catch (Exception e)
-            {   //navigate to /Home/IntentionalError to see the message displayed on the screen and intentionally throw the exception
-
+            {   
                 Log.Error(e, "Unhandled exception occurred."); //logging error using Serilog
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError; //noticed that without manually setting the status, upon inspecting it showed the status as "200 OK" even though an exception is there.
                 await context.Response.WriteAsync("Something went wrong! Please try again later.");
